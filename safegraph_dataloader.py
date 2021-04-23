8cc1deed0b64ba0aa2d11907d9147b2a5c416d7a
Marios Papachristou <papachristoumarios@gmail.com>
2021-04-23 13:24:27 -0400

    Improvements

--- safegraph_dataloader.py
+++ safegraph_dataloader.py
@@ -13,6 +13,7 @@ import seaborn as sns
 import argparse
 import os
 import sys
+from stimuli import *
 sns.set_theme()
 
 def get_argparser():
@@ -21,6 +22,10 @@ def get_argparser():
     parser.add_argument('--patterns_data', default='/home/mp2242/safergraph-monthly')
     parser.add_argument('--loans_data', default='/home/mp2242/safergraph-loans')
     parser.add_argument('--n_neighbors', default=5, type=int)
+    parser.add_argument('--payroll_data', default='/home/mp2242/payroll')
+    parser.add_argument('--expenditures_data', default='/home/mp2242/consumer-expenditures')
+    parser.add_argument('--lat', default=42.439499559524485, type=float)
+    parser.add_argument('--lon', default=-76.49499020058316, type=float)
 
     return parser.parse_args()
 
@@ -59,98 +64,76 @@ def dwell_workers(u):
 
     return json.dumps(result)
 
-def create_multi_graph(patterns, households_dependents, households_race):
+def create_multi_graph(patterns, expenditures):
 
     G = nx.MultiDiGraph()
-    multi_edges = collections.defaultdict(int)
-    dist_dependents = collections.defaultdict(int)
-    dist_minority = collections.defaultdict(float)
-    households_race['p_minority'] = households_race['minority'] / (households_race['minority'] + households_race['white'])
+    expenditures_freq = collections.defaultdict(lambda: collections.defaultdict(float))
 
     for _, x in patterns.iterrows():
+        naics_code_prefix = int(str(x['naics_code'])[:2])
         workers = json.loads(x['workers'])
         home_cbg = json.loads(x['visitor_home_cbgs'])
+        for cbg, val in home_cbg.items():
+            cbg_workers = int(round(workers['workers'] * val))
+            cbg_non_workers = int(val - cbg_workers)
+            
+            if not expenditures[expenditures['naics_code_prefix'] == naics_code_prefix].empty:
+                expenditures_freq[cbg][naics_code_prefix] += 1
+            else:
+                expenditures_freq[cbg][0] += 1
+
+            [G.add_edge(x['placekey'], int(cbg), weight=x['payroll']) for _ in range(cbg_workers)]
+
+
+    for _, x in patterns.iterrows():
+        naics_code_prefix = int(str(x['naics_code'])[:2])
+        workers = json.loads(x['workers'])
+        home_cbg = json.loads(x['visitor_home_cbgs'])
+        try:
+            expenditure_naics = expenditures[expenditures['naics_code_prefix'] == naics_code_prefix]['monthly'].iloc[0]
+        except:
+            expenditure_naics = expenditures[expenditures['naics_code_prefix'] == 0]['monthly'].iloc[0]
+            naics_code_prefix = 0
 
         for cbg, val in home_cbg.items():
             cbg_workers = int(round(workers['workers'] * val))
             cbg_non_workers = int(val - cbg_workers)
-            if G.has_node(cbg):
-                G.add_node(cbg, dist_minority = households_race[housholds_race['census_block_group'] == cbg]['p_minority'].iloc[0], dist_dependents = list(households_dependents[households_dependents['census_block_group'] == cbg][['B11016e2', 'B11016e3', 'B11016e4', 'B11016e5', 'B11016e6', 'B11016e7', 'B11016e8']].iloc[0]))
 
-            [G.add_edge(int(cbg), x['placekey']) for _ in range(cbg_non_workers)]
-            [G.add_edge(x['placekey'], int(cbg)) for _ in range(cbg_workers)]
+            weight = cbg_non_workers * expenditure_naics / expenditures_freq[cbg][naics_code_prefix] 
+            [G.add_edge(int(cbg), x['placekey'], weight=weight) for _ in range(cbg_non_workers)]
 
     return G
 
-def create_graph(patterns, households_dependents, households_race):
 
-    multi_edges = collections.defaultdict(int)
-    num_dependents = collections.defaultdict(int)
-    is_minority = collections.defaultdict(int)
+def create_eisenberg_noe_data(G):
+    
+    n = len(G)
+    P = np.zeros(shape=(n, n))
+    L = np.zeros(shape=(n, 1))
+    C = np.zeros(shape=(n, 1))
+    B = np.zeros(shape=(n, 1))
+    A = np.zeros(shape=(n, n))
 
-    for _, x in patterns.iterrows():
-        workers = json.loads(x['workers'])
-        home_cbg = json.loads(x['visitor_home_cbgs'])
+    G = nx.relabel.convert_node_labels_to_integers(G)
 
-        for cbg, val in home_cbg.items():
-            cbg_workers = int(round(workers['workers'] * val))
-            cbg_non_workers = int(val - cbg_workers)
+    for u, v, data in G.edges(data=True):
+        P[u, v] += data.get('weight', 0)
 
-            if cbg_non_workers > 0:
-                multi_edges[int(cbg), x['placekey']] += cbg_non_workers
-            if cbg_workers > 0:
-                multi_edges[x['placekey'], int(cbg)] += cbg_workers
+    for u, data in G.nodes(data=True):
+        L[u] += data.get('L', 0)
+        C[u] += data.get('assets', 0)
+        B[u] += data.get('liabilities', 0)
 
+    P_bar = B + P.sum(0).reshape(n, 1)
 
-    G = nx.DiGraph()
-    idx_2_node_from = {}
-    idx_2_node_to = {}
-    counter_from = 0
-    counter_to = 0
-    households_race['p_minority'] = households_race['minority'] / (households_race['minority'] + households_race['white'])
-    multi_edges_keys = list(multi_edges.keys())
+    A = np.copy(P)
+    for i in range(n):
+        A[i] /= P_bar[i]
 
-
-    for (u, v) in multi_edges_keys:
-
-        if isinstance(u, int) and isinstance(v, str):
-            idx = 0
-            weights = list(households_dependents[households_dependents['census_block_group'] == u][['B11016e2', 'B11016e3', 'B11016e4', 'B11016e5', 'B11016e6', 'B11016e7', 'B11016e8']].iloc[0])
-
-            p_minority = households_race[households_race['census_block_group'] == u]['p_minority'].iloc[0]
-            for j in range(multi_edges[u, v]):
-                G.add_edge('{}_{}'.format(u, idx), v)
-                idx_2_node_from[counter_from] = '{}_{}'.format(u, idx)
-                counter_from += 1
-                num_dependents['{}_{}'.format(u, idx)] = random.choices([1, 2, 3, 4, 5, 6, 7], weights=weights, k=1)[0]
-                is_minority['{}_{}'.format(u, idx)] = np.random.uniform() <= p_minority
-                idx += 1
-            # Dependent or independent?
-            for j in range(multi_edges[v, u]):
-                G.add_edge(v, '{}_{}'.format(u, idx))
-                idx_2_node_to[counter_to] = '{}_{}'.format(u, idx)
-                counter_to += 1
-                num_dependents['{}_{}'.format(u, idx)] = random.choices([1, 2, 3, 4, 5, 6, 7], weights=weights, k=1)[0]
-                is_minority['{}_{}'.format(u, idx)] = np.random.uniform() <= p_minority
-                idx += 1
-
-
-    H = nx.DiGraph(nx.scale_free_graph(counter_from + counter_to))
-
-    for (u, v) in H.edges():
-        if u < counter_from:
-            uu = idx_2_node_from[u]
-        else:
-            uu = idx_2_node_to[u - counter_from]
-
-        if v < counter_from:
-            vv = idx_2_node_from[v]
-        else:
-            vv = idx_2_node_to[v - counter_from]
-
-        G.add_edge(uu, vv)
-
-    return G, num_dependents
+    print('external liabilities')
+    print(B)
+    print('external assets')
+    print(C)
 
 args = get_argparser()
 
@@ -160,9 +143,7 @@ X = np.vstack([geographic_data['latitude'].to_numpy(), geographic_data['longitud
 knn = NearestNeighbors(n_neighbors=args.n_neighbors, metric='haversine')
 knn.fit(X)
 
-#_, indices = knn.kneighbors([[42.439499559524485, -76.49499020058316]])
-
-_, indices = knn.kneighbors([[40.834998005599736, -73.9917002004307]])
+_, indices = knn.kneighbors([[args.lat, args.lon]])
 
 # Pick k-nearest cbgs according to haversine distance from a user-given (lat, lon) pair
 neighbor_cbgs = geographic_data.iloc[indices[0].tolist()]
@@ -172,8 +153,17 @@ neighbor_cbgs = list(neighbor_cbgs['census_block_group'])
 brands = pd.read_csv(os.path.join(args.patterns_data, 'brand_info_backfill/2020/12/13/04/2018/03/brand_info.csv'))
 brands.rename(columns={'safegraph_brand_id' : 'safegraph_brand_ids'}, inplace=True)
 
-brands = brands[['safegraph_brand_ids', 'top_category']]
+brands = brands[['safegraph_brand_ids', 'naics_code']]
 brands.set_index('safegraph_brand_ids', inplace=True)
+
+# Load payroll data
+payroll = pd.read_csv(os.path.join(args.payroll_data, 'employment_clean.csv'))
+payroll.rename(columns={'NAICS' : 'naics_code', 'PAYROLL' : 'payroll'}, inplace=True)
+
+# Join payroll data
+brands = brands.join(payroll.set_index('naics_code'), on='naics_code', how='left')
+
+brands.fillna(value={'payroll' : brands['payroll'].mean()}, inplace=True)
 
 # Monthly patterns
 patterns_helper = []
@@ -193,7 +183,7 @@ patterns = pd.concat(patterns_helper, axis=0, ignore_index=True)
 patterns = patterns.join(brands, on='safegraph_brand_ids')
 
 # Drop unneeded columns
-patterns = patterns[['placekey', 'top_category', 'poi_cbg', 'visitor_home_cbgs', 'bucketed_dwell_times']]
+patterns = patterns[['placekey', 'naics_code', 'payroll', 'poi_cbg', 'visitor_home_cbgs', 'bucketed_dwell_times']]
 
 # Estimate the probability of a device belonging to a worker (resp. a non-worker)
 patterns['workers'] = patterns['bucketed_dwell_times'].apply(dwell_workers)
@@ -204,8 +194,100 @@ households = pd.read_csv(os.path.join(args.census_data, 'data/cbg_b11.csv'))
 households_race = households[['census_block_group', 'B11001Ae1', 'B11001Ie1', 'B11001Be1', 'B11001Ce1', 'B11001De1', 'B11001Ee1', 'B11001Fe1']]
 households_race.rename(inplace=True, columns={'B11001Ae1': 'white', 'B11001Ie1': 'hispanic/latino', 'B11001Be1' : 'african_american', 'B11001Ce1' : 'native/indian', 'B11001De1' : 'asian', 'B11001Ee1' : 'hawaian', 'B11001Fe1': 'other'})
 households_race['minority'] = households_race['hispanic/latino'] + households_race['african_american'] + households_race['native/indian'] + households_race['asian'] + households_race['hawaian'] + households_race['other']
-households_race.set_index('census_block_group')
+households_race['p_minority'] = households_race['minority'] / (households_race['minority'] + households_race['white'])
+households_race.set_index('census_block_group', inplace=True)
 households_dependents = households[['census_block_group', 'B11016e2', 'B11016e3', 'B11016e4', 'B11016e5', 'B11016e6', 'B11016e7', 'B11016e8']]
-households.set_index('census_block_group')
+households_dependents.set_index('census_block_group', inplace=True)
 
-G = create_multi_graph(patterns, households_dependents, households_race)
+
+# Household income
+households_income = pd.read_csv(os.path.join(args.census_data, 'data/cbg_b19.csv'))
+households_income = households_income.loc[:,~households_income.columns.str.contains('m', case=False)]
+households_income['annual_income'] = households_income.loc[:, 'B19052e2':'B19060e3'].sum(axis=1) 
+
+# Employment status
+households_employment = pd.read_csv(os.path.join(args.census_data, 'data/cbg_b23.csv'))
+households_employment['p_employed'] = households_employment['B23025e2'] / households_employment['B23025e2']
+households_employment = households_employment[['census_block_group', 'p_employed']]
+households_employment.set_index('census_block_group', inplace=True)
+
+# Business Bailouts
+loans = pd.read_csv(os.path.join(args.loans_data, 'loans.csv'))
+loans.rename(inplace=True, columns={'placekey_poi_match' : 'placekey'})
+loans.set_index('placekey', inplace=True)
+loans = loans[loans.index.isin(patterns['placekey'])]
+
+# Consumer Expenditures
+expenditures = pd.read_csv(os.path.join(args.expenditures_data, 'expenditures.csv'))
+
+# Create multi-graph topology
+G = create_multi_graph(patterns, expenditures)
+
+# Create node attributes
+loans_amount = loans['amount'] / 12
+loans_amount = loans['amount'].to_dict()
+nx.set_node_attributes(G, loans_amount, 'L')
+
+loans_race = loans['race'].to_dict()
+nx.set_node_attributes(G, loans_race, 'race')
+
+households_dependents = households_dependents[households_dependents.index.isin(list(G.nodes()))]
+households_dependents_avg = collections.defaultdict(float)
+num_households = collections.defaultdict(int)
+households_bailouts = collections.defaultdict(float)
+households_external_assets = collections.defaultdict(float)
+
+annual_expenditures = 63000
+monthly_expenditures = annual_expenditures / 12
+households_external_liabilities = collections.defaultdict(float)
+
+for idx, dist in households_dependents.iterrows():
+    if np.sum(list(dist)) > 0:
+        try:
+            p_employed = households_employment[households_employment['census_block_group'] == idx]['p_employed'].iloc[0]
+        except:
+            p_employed = 1
+        households_dependents_avg[idx] = np.average(np.arange(1, 1 + len(dist)), weights=list(dist))
+        num_households[idx] = int(np.ceil((G.in_degree(idx) / p_employed + G.out_degree(idx)) / households_dependents_avg[idx]))
+    else:
+        households_dependents_avg[idx] = 1
+        num_households[idx] = int(np.ceil(G.in_degree(idx) + G.out_degree(idx)))
+
+    annual_income = households_income[households_income['census_block_group'] == idx]['annual_income'].iloc[0]
+    monthly_income = annual_income / 12
+    households_bailouts[idx] = num_households[idx] * stimulus_check(annual_income, households_dependents_avg[idx])
+
+    total_weight = 0
+    for v in G.predecessors(idx):
+        for key, val in G.get_edge_data(v, idx).items():
+            total_weight += G[v][idx][key].get('weight', 0)
+
+    households_external_assets[idx] = max(0, num_households[idx] * monthly_income - total_weight)
+
+
+nx.set_node_attributes(G, households_dependents_avg, 'avg_dependents')
+nx.set_node_attributes(G, num_households, 'num_people')
+nx.set_node_attributes(G, households_race['p_minority'].to_dict(), 'p_minority')
+nx.set_node_attributes(G, households_bailouts, 'L')
+nx.set_node_attributes(G, households_external_assets, 'assets')
+
+for x in G.nodes():
+    if isinstance(x, str):
+        for cbg in G.predecessors(x):
+            for key, val in G.get_edge_data(cbg, x).items():
+                G[cbg][x][key]['weight'] = G[cbg][x][key]['weight'] / households_dependents_avg[cbg]
+
+for cbg in G.nodes():
+    if isinstance(cbg, int):
+        for x in G.successors(cbg):
+            total_weight = 0
+            for key, val in G.get_edge_data(cbg, x).items():
+                total_weight += G[cbg][x][key]['weight']
+
+            households_external_liabilities[cbg] = 1000 + max(0, num_households[cbg] * monthly_expenditures - total_weight)
+
+nx.set_node_attributes(G, households_external_liabilities, 'liabilities')
+
+create_eisenberg_noe_data(G)
+nx.write_gpickle(G, "safegraph.gpickle")
+
